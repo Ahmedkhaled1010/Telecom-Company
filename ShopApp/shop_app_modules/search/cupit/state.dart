54c30384ce14c4b82423ac968d8a7f405b27d01e
Ahmed Khaled <62180111+Ahmedkhaled1010@users.noreply.github.com>
2024-01-07 04:50:10 +0200

    Delete ShopApp directory

--- ShopApp/shop_app_modules/search/cupit/state.dart
+++
@@ -1,6 +0,0 @@
-abstract class SearchState{}
-
-class SearchInitialState extends SearchState{}
-class SearchLoadingState extends SearchState{}
-class SearchSuccessState extends SearchState{}
-class SearchErorrState extends SearchState{}
